bbadf5864d9b1e818d692eeafbdbebdfe6ea8da3
rick427 <44168932+rick427@users.noreply.github.com>
2019-07-31 11:50:58 +0100

    breaking into components and styling

---
+++ src/components/menu/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './menu.styles.scss';
+
+const Menu = ({title, imageUrl, size}) => (
+    <div style={{backgroundImage: `url(${imageUrl})`}} className={`${size} menu-item`}>
+        <div className="content">
+            <h1 className="title">{title}</h1>
+            <span className="subtitle">SHOP NOW</span>
+        </div>
+    </div>
+)
+
+export default Menu;
---
+++ src/components/pages/homepage/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../../directory/Directory';
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <Directory/>
+        </div>
+    )
+}
+
+export default HomePage;
