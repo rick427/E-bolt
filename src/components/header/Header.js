4881f95eb8aca5688c1f2cae3fe6a49776678a45
rick427 <44168932+rick427@users.noreply.github.com>
2019-08-03 15:57:13 +0100

    mapping currentUser as props to Header compt

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import {NavLink, Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
@@ -29,4 +30,8 @@ const Header = ({currentUser}) => {
     )
 }
 
-export default Header;
+const mapStateToProps = state => ({ //state here is the rootReducer
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
