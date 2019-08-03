8353b5b1449e6298235dc7c0f075baa0abf797ef
rick427 <44168932+rick427@users.noreply.github.com>
2019-08-03 15:51:40 +0100

    setting up redux 2

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
+
 import './index.css';
 import App from './App';
+import store from './redux/store';
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
