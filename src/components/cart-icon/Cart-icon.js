ddb78cc8bc946c03c29795edaa3b80b9f6614c2e
rick427 <44168932+rick427@users.noreply.github.com>
2019-08-04 18:31:17 +0100

    getting the total quantity of products in cart

--- src/components/cart-icon/Cart-icon.js
+++ src/components/cart-icon/Cart-icon.js
@@ -4,17 +4,21 @@ import {toggleCartHidden} from '../../redux/cart/cart-actions';
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import './cart-icon.scss';
 
-const CartIcon = ({toggleCartHidden}) => {
+const CartIcon = ({toggleCartHidden, itemCount}) => {
     return (
         <div className="cart-icon" onClick={toggleCartHidden}>
             <ShoppingIcon className="shopping-icon"/>
-            <span className="item-count">0</span>
+            <span className="item-count">{itemCount}</span>
         </div>
     )
 }
+
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    itemCount: cartItems.reduce((totalQuantity, cartItem) => totalQuantity + cartItem.quantity, 0)
+})
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
