401c12361b07d5febcf94aa2b3141225063d2377
rick427 <44168932+rick427@users.noreply.github.com>
2019-08-09 13:18:25 +0100

    adding total value selector  for checkout

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+import {selectCartItems, selectCartTotal} from '../../redux/cart/cart-selector';
 import './checkout.scss';
 
-const Checkout = () => {
+const Checkout = ({cartItems, total}) => {
     return (
-        <div>
-           CHECKOUT PAGE 
+        <div className="checkout-page">
+           <div className="checkout-header">
+               <div className="header-block">
+                   <span>Product</span>
+               </div>
+               <div className="header-block">
+                   <span>Description</span>
+               </div>
+               <div className="header-block">
+                   <span>Quantity</span>
+               </div>
+               <div className="header-block">
+                   <span>Price</span>
+               </div>
+               <div className="header-block">
+                   <span>Remove</span>
+               </div>
+           </div>
+           {cartItems.map(cartItem => cartItem.name)}
+
+           <div className="total">
+               <span>TOTAL: ${total}</span>
+           </div>
         </div>
     )
 }
 
-export default Checkout;
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems,
+    total: selectCartTotal
+})
+
+export default connect(mapStateToProps)(Checkout);
--- src/redux/cart/cart-selector.js
+++ src/redux/cart/cart-selector.js
@@ -16,3 +16,8 @@ export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems => cartItems.reduce((totalQuantity, cartItem) => totalQuantity + cartItem.quantity, 0)
 )
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce((totalQuantity, cartItem) => totalQuantity + cartItem.quantity * cartItem.price, 0)
+)
