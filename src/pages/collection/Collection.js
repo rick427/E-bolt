6779a2c22a8a737dd6ef980a77ab4951e1ad6d35
rick427 <44168932+rick427@users.noreply.github.com>
2019-08-09 16:26:26 +0100

    data normalization on shop data & collections page

--- src/pages/collection/Collection.js
+++ src/pages/collection/Collection.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {selectCollection} from '../../redux/shop/shop-selector'
 import './collection.scss';
+import CollectionItem from '../../components/collection-item/CollectionItem';
 
 const CollectionPage = ({collection}) => {
-  console.log(collection)
+    const {title, items} = collection
     return (
-        <div className="category">
-          <h2>COLLECTION PAGE</h2>  
+        <div className="collection-page">
+          <h2 className="title">{title}</h2> 
+          <div className="items">
+            {items.map(item => <CollectionItem key={item.id} item={item} />)}
+          </div>
         </div>
     )
 }
