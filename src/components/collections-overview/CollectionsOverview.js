8b1fb2eb10ef3bea643ccba8146d6331db4ba7c4
rick427 <44168932+rick427@users.noreply.github.com>
2019-08-09 16:40:14 +0100

    updated shop selector for collection overview

--- src/components/collections-overview/CollectionsOverview.js
+++ src/components/collections-overview/CollectionsOverview.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
-import {selectCollections} from '../../redux/shop/shop-selector'
+import {selectCollectionsForPreview} from '../../redux/shop/shop-selector'
 import CollectionPreview from '../collection-preview/CollectionPreview';
 import  './collections-overview.scss';
 
@@ -17,7 +17,7 @@ const CollectionsOverview = ({collections}) => {
 }
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
   })
 
 export default connect(mapStateToProps)(CollectionsOverview);
