259b722aaa0b8b406c561238e445d747552290ed
rick427 <44168932+rick427@users.noreply.github.com>
2019-08-13 13:57:31 +0100

    finishing stripe payment

--- client/src/components/stripe-button/StripeButton.js
+++ client/src/components/stripe-button/StripeButton.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import logo from '../../assets/crown.svg';
+import axios from 'axios';
 
 const StripeButton = ({price}) => {
     const stripePrice = price * 100;
     const publishableKey = 'pk_test_pBLOqR9tg3g5oO9CLPomP6JG00AkyeQRDi'
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful')
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: stripePrice,
+                token
+            }
+        })
+        .then(res => alert('Payment Successful'))
+        .catch(error => {
+            console.log('Payment error', JSON.parse(error))
+            alert('There was an issue with your payment. Please make sure you use the provided credit card.')
+        })
     }
 
     return (
